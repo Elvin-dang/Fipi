63e61d0ae237d20546a94a1eb68377ebd4f77876
Elvin Dang <xdan0772@uni.sydney.edu.au>
2025-01-20 15:19:55 +0700

    (Term) 1

--- src/app/(server)/room/route.ts
+++ src/app/(server)/room/route.ts
@@ -4,4 +4,5 @@ import { geolocation, ipAddress } from "@vercel/functions";
 export function GET(req: NextRequest) {
   const geo = geolocation(req);
   const ip = ipAddress(req);
+  console.log(geo, ip);
 }
