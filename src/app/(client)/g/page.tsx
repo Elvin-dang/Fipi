fb3ad51869b8532cf4e418c45d0137bc9a6efe51
Elvin Dang <xdan0772@uni.sydney.edu.au>
2025-02-18 15:20:23 +1100

    Update VERCEL url

--- src/app/(client)/g/page.tsx
+++ src/app/(client)/g/page.tsx
@@ -4,7 +4,7 @@ import Room from "../_components/Room/Room";
 export const dynamic = "force-dynamic";
 
 const page = async () => {
-  const data = await fetch(`${process.env.URL}/room`);
+  const data = await fetch(`${process.env.VERCEL_URL || process.env.URL}/room`);
   const { name } = await data.json();
 
   return <Room roomId={name} />;
