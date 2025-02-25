36507613ed1815c7cf973066b75b52574b23bcc0
Elvin Dang <xdan0772@uni.sydney.edu.au>
2025-02-25 20:34:59 +1100

    (G Page) make it rsc

--- src/app/(client)/g/page.tsx
+++ src/app/(client)/g/page.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import Room from "../_components/Room/Room";
+import { headers } from "next/headers";
+import crypto from "crypto";
 
 const Page = async () => {
-  const url =
-    process.env.NODE_ENV === "production"
-      ? `https://${process.env.NEXT_PUBLIC_URL}/room`
-      : `http://${process.env.NEXT_PUBLIC_URL}/room`;
-  const data = await fetch(url, { cache: "no-store" });
-  const jsonData = await data.json();
+  const headersList = await headers();
 
-  return <Room roomId={jsonData.name} type="public" />;
+  const ip =
+    headersList.get("X-Forwarded-For") ||
+    headersList.get("X-Real-IP") ||
+    headersList.get("cf-connecting-ip");
+  // @ts-expect-error ip always exists
+  const name = crypto.createHmac("md5", process.env.SECRET).update(ip).digest("hex");
+
+  return <Room roomId={name} type="public" />;
 };
 
 export default Page;
